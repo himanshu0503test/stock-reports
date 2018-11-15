74a4d6bab37239e24f6024ed7bfea13b4b25d445
himanshu0503test <coderscode@gmail.com>
2018-11-15 21:27:59 +0530

    Update statsCollector.sh

--- statsCollector.sh
+++ statsCollector.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DATE=`date +%Y%m%d`
 
-curl https://www.nseindia.com/live_market/dynaContent/live_analysis/oi_spurts/topPositiveOIChangeData.json > oi/$DATE.json
+curl https://www.nseindia.com/live_market/dynaContent/live_analysis/oi_spurts/topPositiveOIChangeData.json > oi/$DATE.csv
 curl https://nseindia.com/archives/equities/mto/MTO_`date +%d%m%Y`.DAT > delivery/$DATE.csv
 
 #Parsing Delivery File Correctly
@@ -10,9 +10,9 @@ sed '1,4d' delivery/$DATE.csv >> temp.csv
 mv temp.csv delivery/$DATE.csv
 
 #Parsing OI File Correctly
-echo "Symbol, LatestOI, PrevOI, OIChange, %OIChange, Volume, IsFO" > temp
-cat oi/$DATE.json | jq -r '.data[] | [.symbol, .latestOI, .prevOI, .oiChange, .percOIchange, .volume, .isFO] | @csv' > temp
-mv temp oi/$DATE.json
+echo "Symbol, LatestOI, PrevOI, OIChange, %OIChange, Volume, IsFO" > temp.csv
+cat oi/$DATE.csv | jq -r '.data[] | [.symbol, .latestOI, .prevOI, .oiChange, .percOIchange, .volume, .isFO] | @csv' >> temp.csv
+mv temp oi/$DATE.csv
 
 git config credential.helper 'cache --timeout=120'
 git config user.email "<email>"
