77e2dacfc5c12a1a5f9c8478e2d903403637c527
himanshu0503test <coderscode@gmail.com>
2018-11-15 21:18:15 +0530

    Update statsCollector.sh

--- statsCollector.sh
+++ statsCollector.sh
@@ -4,7 +4,8 @@ DATE=`date +%Y%m%d`
 curl https://www.nseindia.com/live_market/dynaContent/live_analysis/oi_spurts/topPositiveOIChangeData.json > oi/$DATE.json
 curl https://nseindia.com/archives/equities/mto/MTO_`date +%d%m%Y`.DAT > delivery/$DATE.csv
 
-sed '1,3d' delivery/$DATE.csv > temp.csv
+echo "Record Type,Sr No,Name of Security, Type of Security, Quantity Traded,Deliverable Quantity(gross across client level),% of Deliverable Quantity to Traded Quantity" > temp.csv
+sed '1,4d' delivery/$DATE.csv >> temp.csv
 mv temp.csv delivery/$DATE.csv
 
 git config credential.helper 'cache --timeout=120'
