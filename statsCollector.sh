2c841d2770e5a3d176528501801f6c304c28d7e4
himanshu0503test <coderscode@gmail.com>
2018-11-15 23:36:44 +0530

    Add logic for f&o stocks

--- statsCollector.sh
+++ statsCollector.sh
@@ -3,17 +3,22 @@
 DATE=`date +%Y%m%d`
 
 curl https://www.nseindia.com/live_market/dynaContent/live_analysis/oi_spurts/topPositiveOIChangeData.json > oi/$DATE.csv
-curl https://nseindia.com/archives/equities/mto/MTO_`date +%d%m%Y`.DAT > delivery/$DATE.csv
 
-#Parsing Delivery File Correctly
+#Delivery
 echo "Record Type,Sr No,Name of Security, Type of Security, Quantity Traded,Deliverable Quantity(gross across client level),% of Deliverable Quantity to Traded Quantity" > temp.csv
 sed '1,4d' delivery/$DATE.csv >> temp.csv
 mv temp.csv delivery/$DATE.csv
 
-#Parsing OI File Correctly
+#OI
 echo "Symbol, LatestOI, PrevOI, OIChange, %OIChange, Volume, IsFO" > temp.csv
-cat oi/$DATE.csv | jq -r '.data[] | [.symbol, .latestOI, .prevOI, .oiChange, .percOIchange, .volume, .isFO] | @csv' >> temp.csv
+curl https://nseindia.com/archives/equities/mto/MTO_`date +%d%m%Y`.DAT | jq -r '.data[] | [.symbol, .latestOI, .prevOI, .oiChange, .percOIchange, .volume, .isFO] | @csv' >> temp.csv
 mv temp.csv oi/$DATE.csv
+
+#OHL/FO
+echo "Symbol, Open, High, Low, LTP, Change(InPts), Change(%), 52 Week High, 52 Week Low, Change in 365 days (%), Change in 30 days(%), Ex-Dividend-Date, Divident" > temp.csv
+curl https://www.nseindia.com/live_market/dynaContent/live_watch/stock_watch/foSecStockWatch.json | jq -r '.data[] | [.symbol, .open, .high, .low, .ltP, .ptsC, .per, .wkhi, .wklo, .yPC, .mpC, .xDt, .cAct] | @csv' | sort >> temp.csv
+mv temp.csv ohl/fo/$DATE.csv
+
 
 git config credential.helper 'cache --timeout=120'
 git config user.email "<email>"
