5599b48e4f1bcbbf01e2ab2d32528cc5d17cd627
himanshu0503test <coderscode@gmail.com>
2018-11-15 20:53:38 +0530

    Create statsCollector.sh

---
+++ statsCollector.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+DATE=`date +%Y%m%d`
+
+mkdir oi delivery
+
+curl https://www.nseindia.com/live_market/dynaContent/live_analysis/oi_spurts/topPositiveOIChangeData.json > oi/$DATE.json
+curl https://nseindia.com/archives/equities/mto/MTO_`date +%d%m%Y`.DAT > delivery/$DATE.csv
+
+git config credential.helper 'cache --timeout=120'
+git config user.email "<email>"
+git config user.name "Bot"
+git add .
+git commit -m "Data for $DATE"
+git push -q https://${REPORTS_PERSONAL_TOKEN}@github.com/himanshu0503test/stock-reports.git master
