d30fd27b5d2b8e83ced1cd6025c4457f4d51aa3c
himanshu0503test <coderscode@gmail.com>
2018-11-15 21:15:23 +0530

    Update statsCollector.sh

--- statsCollector.sh
+++ statsCollector.sh
@@ -4,6 +4,9 @@ DATE=`date +%Y%m%d`
 curl https://www.nseindia.com/live_market/dynaContent/live_analysis/oi_spurts/topPositiveOIChangeData.json > oi/$DATE.json
 curl https://nseindia.com/archives/equities/mto/MTO_`date +%d%m%Y`.DAT > delivery/$DATE.csv
 
+sed '1,3d' delivery/$DATE.csv > temp.csv
+mv temp.csv delivery/$DATE.csv
+
 git config credential.helper 'cache --timeout=120'
 git config user.email "<email>"
 git config user.name "Bot"
